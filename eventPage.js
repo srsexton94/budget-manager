10fb6377134699a72c8c716ca6bced236c077464
srsexton94 <srsexton94@gmail.com>
2020-05-09 17:45:45 -0400

    Add logic to context menu, add selection to total
    Add function logic so that the user can right click to add a selected
    amount in the browser to their total tally

--- eventPage.js
+++ eventPage.js
@@ -5,3 +5,29 @@ const contextMenuItem = {
 }
 
 chrome.contextMenus.create(contextMenuItem)
+
+chrome.contextMenus.onClicked.addListener(function (clickData) {
+  if (clickData.menuItemId === "spendMoney" && clickData.selectionText) {
+    if (!isNaN(clickData.selectionText)) {
+      chrome.storage.sync.get(['total', 'limit'], budget => {
+        let newTotal = 0
+        if (budget.total) {
+          newTotal += parseFloat(budget.total)
+        }
+        newTotal += parseFloat(clickData.selectionText)
+        chrome.storage.sync.set({ 'total': newTotal }, () => {
+          if (newTotal >= budget.limit) {
+            let notifOptions = {
+              type: 'basic',
+              iconUrl: 'images/calc_48.png',
+              title: 'Limit reached!',
+              message: "Uh oh! Looks like you've reached your limit!"
+            }
+            // uses `notifOptions` object to create the chrome notification
+            chrome.notifications.create('limitNotif', notifOptions)
+          }
+        })
+      })
+    }
+  }
+})
