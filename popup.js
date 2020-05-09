bf7aefbc9de9750a57e5a09448c08630f49ad5d6
srsexton94 <srsexton94@gmail.com>
2020-05-09 15:23:18 -0400

    Add logic to increment spending total

--- popup.js
+++ popup.js
@@ -1,5 +1,17 @@
-$(function() {
-  $('#name').keyup(function () {
-    $('#greet').text(`Hello ` + $('#name').val())
+$(() => {
+  $('#spendAmount').on('click', () => {
+    chrome.storage.sync.get('total', budget => {
+      let newTotal = 0
+      if (budget.total) {
+        newTotal += parseFloat(budget.total)
+      }
+      const amount = $('#amount').val()
+      if (amount) {
+        newTotal += parseFloat(amount)
+      }
+      chrome.storage.sync.set({ 'total': newTotal })
+      $('#total').text(newTotal)
+      $('#amount').val('')
+    })
   })
 })
